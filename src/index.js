654df789807f84a7aa58f2c31df37420937bb08e
pritamduttaofficial <pritamdutta850@gmail.com>
2024-05-24 14:15:04 +0530

    db connection & utilities added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express();
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at PORT ${PORT}`);
+dotenv.config({
+  path: "./env",
 });
 
-export { app };
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed!!", error);
+  });
