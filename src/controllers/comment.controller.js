a8c6cce2b1bdd22544745f446e030f3c7279f00c
pritamduttaofficial <pritamdutta850@gmail.com>
2024-06-02 12:29:33 +0530

    Comment Controllers Added

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,22 +4,127 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+// ------------- get all video comments controller ---------------
 const getVideoComments = asyncHandler(async (req, res) => {
-  //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  // check if videoId exist
+  if (!videoId) {
+    throw new ApiError(400, "VideoId is must to get all comments");
+  }
+
+  // parse page and limit values to integers
+  const pageInt = parseInt(page, 10);
+  const limitInt = parseInt(limit, 10);
+
+  // create a pipeline of match stage to get the video comments
+  const pipeline = [
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+  ];
+
+  const options = {
+    page: pageInt,
+    limit: limitInt,
+  };
+  // execute aggregatePaginate with pipeline output and options for pagination
+  const result = await Comment.aggregatePaginate(
+    Comment.aggregate(pipeline),
+    options
+  );
+
+  if (!result) {
+    throw new ApiError(400, "VideoId is invalid");
+  }
+
+  // return video comments as response
+  return res.status(200).json(
+    new ApiResponse(200, {
+      page: result.page,
+      limit: result.limit,
+      totalPages: result.totalPages,
+      totalDocs: result.totalDocs,
+      docs: result.docs,
+    })
+  );
 });
 
+// ----------------- add comment controller -------------------
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
+  const { content } = req.body;
+  const { videoId } = req.params;
+
+  if (!content) {
+    throw new ApiError(400, "Content is required to add comment");
+  }
+  if (!videoId) {
+    throw new ApiError(400, "VideoId is must to add comments");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(400, "Failed to add comment");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "Comment Added Successfully"));
 });
 
+// ---------------- update comment controller ------------------
 const updateComment = asyncHandler(async (req, res) => {
-  // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "Content is required to update comment");
+  }
+  if (!commentId) {
+    throw new ApiError(400, "CommentId is must to update comments");
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(400, "Comment Updation Failed");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment Updated Successfully"));
 });
 
+// ---------------- delete comment controller ------------------
 const deleteComment = asyncHandler(async (req, res) => {
-  // TODO: delete a comment
+  const { commentId } = req.params;
+  if (!commentId) {
+    throw new ApiError(400, "CommentId is must to delete comments");
+  }
+
+  await Comment.findByIdAndDelete(commentId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment Deleted Successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,7 +6,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: toggle like on video
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
