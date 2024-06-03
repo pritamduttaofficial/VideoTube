f895a1818a686fcf57d7c4a36c0c3b02898078f5
pritamduttaofficial <pritamdutta850@gmail.com>
2024-06-03 19:35:24 +0530

    Dashboard Controllers & Routes Added

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ import likeRouter from "./routes/like.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-// import healthcheckRouter from "./routes/healthcheck.routes.js";
-// import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -35,7 +35,7 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
-// app.use("/api/v1/healthcheck", healthcheckRouter);
-// app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
@@ -6,12 +6,94 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+// ---------------------- get the channel details -----------------------
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid ChannelId");
+  }
+
+  // Total number of subscribers
+  const subscriberCount = await Subscription.countDocuments({
+    channel: channelId,
+  });
+
+  // Total number of videos
+  const videoCount = await Video.countDocuments({ owner: channelId });
+
+  // Aggregate pipeline for total views
+  const viewsPipeline = [
+    { $match: { owner: new mongoose.Types.ObjectId(channelId) } },
+    {
+      $group: {
+        _id: null,
+        totalViews: { $sum: "$views" },
+      },
+    },
+  ];
+  const viewStats = await Video.aggregate(viewsPipeline);
+
+  // Aggregate pipeline for total likes
+  const likesPipeline = [
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    { $unwind: "$likes" },
+    {
+      $group: {
+        _id: null,
+        totalLikes: {
+          $sum: 1,
+        },
+      },
+    },
+  ];
+  const likeStats = await Video.aggregate(likesPipeline);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalSubscribers: subscriberCount,
+        totalVideos: videoCount,
+        totalViews: viewStats[0].totalViews,
+        totalLikes: likeStats[0].totalLikes,
+      },
+      "Channel Stats Fetched Successfully"
+    )
+  );
 });
 
+// ----------------------- get the channel videos -----------------------
 const getChannelVideos = asyncHandler(async (req, res) => {
-  // TODO: Get all the videos uploaded by the channel
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid ChannelId");
+  }
+
+  const videos = await Video.find({ owner: channelId });
+
+  if (!videos.length) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Channel has no videos"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Channel Videos Fetched Successfully"));
 });
 
 export { getChannelStats, getChannelVideos };
